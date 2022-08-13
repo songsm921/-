b61d1e7539b972168ffa7dec8d333ece6a24c38d
songsm921 <90387921+songsm921@users.noreply.github.com>
2022-08-13 13:39:23 +0900

    Change to public Member to Private

--- Assn2_2018/game.h
+++ Assn2_2018/game.h
@@ -19,8 +19,6 @@ class gameInterface{
         int judgeOneGame(int userFinalPick);
         pair<bool,int> isGameOver();
         void calculateLocation(int whoWin);
-        pair<int,int> playerPos;
-        pair<int,int> computerPos;
     private:
         int numberofStairs;
         bool isEven;
@@ -29,6 +27,8 @@ class gameInterface{
         int boardRow;
         int boardColumn;
         int computerFinalPick;
+        pair<int,int> playerPos;
+        pair<int,int> computerPos;
 
         pair<int,int> computerPick;
         string** stairs;
